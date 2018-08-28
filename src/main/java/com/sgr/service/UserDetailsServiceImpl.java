23629652ee73a524f57f3b0985561982c318d144
sagar-kale <sagark.kale@hotmail.com>
2018-08-28 12:28:01 +0530

    added role and commented

--- src/main/java/com/sgr/service/UserDetailsServiceImpl.java
+++ src/main/java/com/sgr/service/UserDetailsServiceImpl.java
@@ -11,7 +11,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -28,6 +30,33 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
         grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
+
+
+    /*
+    String role = user.getRole();
+    List<SimpleGrantedAuthority> authList = getAuthorities(role);
+
+        org.springframework.security.core.userdetails.User user = new org.springframework.security.core.userdetails.User(username, encodedPassword, authList);
+
+        return user;
+   */
+
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+
+    private List<SimpleGrantedAuthority> getAuthorities(String role) {
+        List<SimpleGrantedAuthority> authList = new ArrayList<>();
+        authList.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        //you can also add different roles here
+        //for example, the user is also an admin of the site, then you can add ROLE_ADMIN
+        //so that he can view pages that are ROLE_ADMIN specific
+        if (role != null && role.trim().length() > 0) {
+            if (role.equals("admin")) {
+                authList.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            }
+        }
+
+        return authList;
     }
 }
